b514ae164fbff5fb5ed65fb0d2127a27b0015eee
Zhipeng He <mithrandir0120@gmail.com>
2021-10-12 16:02:06 +1000

    Update ResearchProjectsPage.tsx

--- src/pages/ResearchProjectsPage.tsx
+++ src/pages/ResearchProjectsPage.tsx
@@ -6,7 +6,7 @@ export default function ResearchProjectsPage() {
     const screens = useBreakpoint();
 
     return (
-        <div className={`home-content`} style={{ marginLeft: screens.md ? "15%!important": "0%!important", maxWidth: screens.md ? "70%!important": "100%!important"}}>
+        <div className={`home-content`} style={{ marginLeft: screens.md ? "15%": "0%", maxWidth: screens.md ? "70%": "100%"}}>
             <div className="home-title title">
                 Research Projects
             </div>
